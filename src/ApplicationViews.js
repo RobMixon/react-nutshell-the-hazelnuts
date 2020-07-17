34c0dab84728bbe8964e5c7207a1bb30617d4c13
Sisi Freeley <liusi33@gmail.com>
2020-07-17 09:27:44 -0500

    messages displaying

---
+++ src/ApplicationViews.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import MessageList from "./components/messages/MessageList";
+
+const ApplicationViews = () => {
+    return (
+        <>
+        <Route
+        exact
+        path="/messages"
+        render={props => {
+            return <MessageList {...props} />
+        }}
+        />
+        </>
+    )
+};
+
+export default ApplicationViews
---
+++ src/Nutshell.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ApplicationViews from "./ApplicationViews";
+
+const Nutshell = () => {
+    return (
+        <ApplicationViews />
+    )
+}
+
+export default Nutshell
