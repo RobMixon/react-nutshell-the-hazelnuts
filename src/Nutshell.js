7ce5ea557262f25b87b5cfafd4c016f0f39ccc64
Rob Mixon <mixonroberth@gmail.com>
2020-07-17 09:40:43 -0500

    working on loging

---
+++ src/Nutshell.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import NavBar from "./nav/NavBar";
+import ApplicationViews from "./ApplicationViews";
+import "./NutShell.css";
+
+const NutShell = () => {
+
+  const isAuthenticated = () => sessionStorage.getItem("credentials") !== null;
+
+  const clearUser = () => {
+    sessionStorage.clear()
+    setHasUser(isAuthenticated())
+  }
+
+  const [hasUser, setHasUser] = useState(isAuthenticated());
+
+  const setUser = user => {
+    sessionStorage.setItem("credentials", JSON.stringify(user));
+    setHasUser(isAuthenticated());
+  };
+
+  return (
+    <>
+      <NavBar hasUser={hasUser} clearUser={clearUser} />
+      <ApplicationViews hasUser={hasUser} setUser={setUser} />
+    </>
+  );
+};
+
+export default NutShell;
