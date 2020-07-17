939cd93efa9249b191d511d2d14341d714f1d27f
Rob Mixon <mixonroberth@gmail.com>
2020-07-17 14:11:55 -0500

    merging with chadworth

--- src/components/events/EventCard.js
+++ src/components/events/EventCard.js
@@ -1,1 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./EventCard.css";
+
 // returns HTML for individual Events (cards)
+
+const EventCard = props => {
+    return (
+    <div className="card">
+        <div className="card-content">
+            <h3> title: <span className="card-eventtitle">{props.event.title}</span></h3>
+            <p>Location: {props.event.location}</p>
+            <p>Date: {props.event.date}</p>
+            <button type="button"
+                onClick={() => props.history.push(`/events/${props.event.id}/edit`)}> Edit 
+            </button>
+        </div>
+    </div>
+    )
+}
