0755d336c903e146a691693f21d47260f4e91309
Gavin Swofford <gavinswofford@gmail.com>
2020-07-17 15:54:31 -0500

    merging master

--- src/components/tasks/TaskList.js
+++ src/components/tasks/TaskList.js
@@ -1,28 +1,44 @@
 import TaskManager from "../modules/TaskManager"
 import { Link } from "react-router-dom";
+import TaskCard from "./TaskCard"
 import React, { useState, useEffect } from 'react';
 
-const TaskList = () => {
+const TaskList = (props) => {
     
     const [task, setTask] = useState([])
+   
 
 // get tasks function
+const getTask = () => {
 TaskManager.getAll().then((result) => {
-    // setTask(result)
-    console.log(result)
+    setTask(result)
+    
 })
+}
 
 // console.log(task)
-return (
-<div>Hey</div>
-)
+
 
 // delete task function
 
 // useEffect
-
+useEffect(() => {
+getTask()
+}, []) 
 // return : open form button + ArticleCard function
+return (
+    
+        <div className="container">
+      <h1>Add Task</h1>
 
+      <button type="button" className="addbtn">Add</button>
+      
+      <section className="taskCard">{ task.map(element => 
+        <TaskCard key={element.id} task={element} {...props}/>
+        )};
+        </section>
+    </div>
+    )
 
 }
 
