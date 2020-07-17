72f0cef2e948bd9b4f28d4d966f9d378227c8887
Gavin Swofford <gavinswofford@gmail.com>
2020-07-17 09:42:12 -0500

    merge conflicts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Nutshell from './components/Nutshell';
+// import Nutshell from './Nutshell';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Nutshell />
+    {/* <Nutshell /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
