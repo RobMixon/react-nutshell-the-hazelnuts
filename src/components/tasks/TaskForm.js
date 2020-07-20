e48cf1d605afba8e794354e246db2d85d95ff5ce
Gavin Swofford <gavinswofford@gmail.com>
2020-07-20 11:24:34 -0500

    users now have a form to fill out when to they click add task

--- src/components/tasks/TaskForm.js
+++ src/components/tasks/TaskForm.js
@@ -3,16 +3,60 @@ import TaskManager from '../../components/modules/TaskManager'
 const userId = 2; 
 const TaskForm = (props) => {
 
-    const [task, setTask] = useState({})
-  const [isLoading, setIsLoading] = useState(false);
+    const [task, setTask] = useState({userId: userId, title: "", completeBy: "", status: false})
+    const [isLoading, setIsLoading] = useState(false);
 
 // add task function with userID, task name, due date(manual), complete (default: false)
+const handleFieldChange = evt => {
+    const stateToChange = {...task};
+    stateToChange[evt.target.id] = evt.target.value
+    setTask(stateToChange);
+}
 
+const constructNewTask = evt => {
+    evt.preventDefault();
+    task.title === "" ? window.alert("Please make sure you fill in title"): task.completeBy === "" ? window.alert("please enter a completion date") :
+    setIsLoading(true)
+    TaskManager.post(task).then(() => props.history.push("/tasks"))
+}
 // checkbox to mark complete as true
 
 // save button
 
-return (<div>Hey  Task Form</div>)
+return (
+    <>
+            <form>
+                <fieldset>
+                    <div className="formgrid">
+                        <input
+                            type="text"
+                            required
+                            onChange={handleFieldChange}
+                            id="title"
+                            placeholder="Enter a Title"
+                        />
+                        <label htmlFor="title">Title: </label>
+                        <input
+                            type="text"
+                            required
+                            onChange={handleFieldChange}
+                            id="completeBy"
+                            placeholder="Enter a completion data"
+                        />
+                        <label htmlFor="completeBy">Complete By: </label>
+
+                    </div>
+                    <div className="alignRight">
+                        <button
+                            type="button"
+                            disabled={isLoading}
+                            onClick={constructNewTask}
+                        >Submit</button>
+                    </div>
+                </fieldset>
+            </form>
+        </>
+)
 }
 
 
