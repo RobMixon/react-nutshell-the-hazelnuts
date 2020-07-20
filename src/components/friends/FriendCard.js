1e021a0fe45e81e37659e408485a196c664cdcaa
Sisi Freeley <liusi33@gmail.com>
2020-07-20 15:40:05 -0500

    fixing friend populating issue

--- src/components/friends/FriendCard.js
+++ src/components/friends/FriendCard.js
@@ -1,6 +1,16 @@
 import React from "react";
 
 const FriendCard = (props) => {
+
+  // const showFriends = () => {
+  //   if (props.friend.user === "") {
+  //     return null
+  //   // } else if (props.friend.userId === 1) {
+  //   //   return null
+  //   }else {
+  //     return props.friend.user.username
+  //   }
+  // }
   
     return (
         <>
@@ -8,11 +18,12 @@ const FriendCard = (props) => {
             <div className="friend__userDetails">
               <img className="friend__userImage" src="./userIcon-black.png" alt="userIcon" />
               <div className="friend__name">
-              {props.friend.user === "" ? null : props.friend.user.username}
+              {/* {showFriends} */}
+              {props.friend.user === undefined ? null : props.friend.user.username}
               </div>
             </div>
             <div className="friendsList__deleteBtn">
-              <button id="deleteFriend" className="deleteBtn" type="button">&times;</button>
+              <button id="deleteFriend" className="deleteBtn" type="button" onClick={() => props.deleteFriend(props.friend.id)}>&times;</button>
             </div>
         </div>
     </>
