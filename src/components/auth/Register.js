917407d1059fb10b21018db0f7d549ecd38e6862
Chad Jones <chadjones839@gmail.com>
2020-07-21 20:59:37 -0500

    fixed Registration default image filepath

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -21,7 +21,7 @@ const constructNewUser = evt => {
   } else if (user.password!== password2) {
     window.alert("Your password does not match")
   } else {
-    user.image = "./userIcon-black.png"
+    user.image = "userIcon-black.png"
     setIsLoading(true);
     sessionStorage.setItem("user", JSON.stringify(user))
     props.setUser(user)
